68b5274c4e86a32b8eaf72f0f483763b11ffca6a
adwaitpawar <adwaitpawar@gmail.com>
2024-02-01 15:23:26 +0530

    first commit

---
+++ dbo/Tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [EmployeeID] INT             NOT NULL,
+    [FirstName]  VARCHAR (50)    NULL,
+    [LastName]   VARCHAR (50)    NULL,
+    [Department] VARCHAR (50)    NULL,
+    [Salary]     DECIMAL (10, 2) NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
+);
+
